cd8bed0f147f65083b7c241186a63620176fed2c
dubai <andy@gmail.com>
2022-05-16 16:57:48 +0400

    helloword输出

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * @Classname HelloWorld
+ * @Description
+ * @Date 2022/5/16 16:37
+ * @Created by Andy
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello IDE!");
+    }
+}
